775f521300bdbad402bcae2e66719f645877cf79
gopigane <92424505+gopigane@users.noreply.github.com>
2022-07-05 11:14:40 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  shared_config_files      = ["/root/.aws/config"]
-  shared_credentials_files = ["/root/.aws/credentials"]
+  shared_config_files      = ["/home/ubuntu/.aws/config"]
+  shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
 }
 
 
